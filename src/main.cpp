12b489c1985b654e30dc8f60a97f4a7c3006b642
andrey <andrey@debian>
2016-12-17 17:58:16 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
-// main.cpp
+//============================================================================
+// Name        : main.cpp
+// Date        : 17.12.2016
+// Author      : Andrey Popov
+// Copyright   : All rights reserved
+//============================================================================
+
 #include "Fft.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   std::vector<std::complex<double>> data = {
     1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
